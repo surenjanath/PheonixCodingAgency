ee9782abca12639401c5d2ec2c4b0a02da5cd470
Surenjanath <surenjanath.singh@gmail.com>
2022-10-02 01:22:58 -0400

    initial commit

--- PROJECT/core/urls.py
+++ PROJECT/core/urls.py
@@ -5,6 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include  # add this
+import views 
+
 
 urlpatterns = [
     path("", include("apps.landing.urls")), 
@@ -13,5 +15,11 @@ urlpatterns = [
     path("project/", include("apps.project.urls")) ,
     path("group_project/", include("apps.group_project.urls")) ,
     path("Dashboard/", include("apps.home.urls"))   ,          
+    path("Dashboard/", include("apps.home.urls"))   ,  
+    
+    
+    
+    
+    path("update_server/", views.update, name="update"),        
                 
 ]
